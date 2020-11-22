7a5626342de61df58013ef03ee1196af6ee69914
MacBook Pro <macbookpro@jayasogi.local>
2020-11-22 18:07:09 +0100

    comments added

--- src/main/java/com/simplemath/Calculate.java
+++ src/main/java/com/simplemath/Calculate.java
@@ -2,10 +2,14 @@ package com.simplemath;
 
 public class Calculate implements CalculateInterface {
 
-	
+	/**
+	 * @param limit --The input to print numbers.
+	 * @throws IllegalArgumentException - thrown when the input parameter is less than 0
+	 */
 	public Boolean printNumbers(int limit) {
-		if (limit > 0) {
-			for (int i = 0; i < limit; i++) {
+		if (limit > 0)  
+		   {
+			for (int i = 1; i <= limit; i++) {
 				System.out.print(i + " ");
 			}
 			return true;
@@ -13,6 +17,11 @@ public class Calculate implements CalculateInterface {
 			 throw new IllegalArgumentException("The value to print should be greater than 0");
 	}
 
+	/**
+	 * @param breadth of triangle
+	 * @param height of triange
+	 * @throws  IllegalArgumentException thrown when the input is less than 0
+	 */
 	public Double areaOfTriangle(double breadth, double height) {
 		double area = 0.0;
 		if (breadth > 0 && height > 0)
--- src/test/java/tdd/CalculateTest.java
+++ src/test/java/tdd/CalculateTest.java
@@ -15,27 +15,39 @@ public class CalculateTest {
 	   calculation=new Calculate();
 	}
 	
+	/**
+	 * Positive test to check , printing of numbers
+	 */
 	@Test
 	public void printNumbersTest() {
 		int limit=10;		
 		Assert.assertTrue("The limit is not correct", calculation.printNumbers(limit));
 	}
 	
+	/**
+	 * Negative test to check if the IllegalArgumentException is thrown when given wrong input
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void printNumberWrongInputTest() {	
 		Assert.assertTrue("The limit is not correct", calculation.printNumbers(-10));
 	}
 	
+	/**
+	 * Positive test to check , Area of triangle
+	 */
 	@Test
 	public void areaOfTriangleTest() {
 		double breadth=3.0;
 		double height=4.0;
 		double area= 0.5 *breadth * height;
 		double returnValue=calculation.areaOfTriangle(breadth,height);
-		System.out.println(" Return value="+returnValue);
+		System.out.println("Area of triagle with breadth : "+breadth+" height :"+height+" is "+returnValue);
 		Assert.assertEquals(area, returnValue,0.0);
 	}
 	
+	/**
+	 * Negative test to check if the IllegalArgumentException is thrown when given wrong input
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void areaOfTriangleWithNegativeInputTest() {
 		double returnValue=calculation.areaOfTriangle(0,4);
